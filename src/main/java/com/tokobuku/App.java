2368d0d5cadf5d0181417ad37df79783ef6d111d
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-09-29 07:59:03 +0700

    Penerapan JdbcDaoSupport

--- src/main/java/com/tokobuku/App.java
+++ src/main/java/com/tokobuku/App.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Main.xml");
-        CustomerDao customerDao = (CustomerDao) context.getBean("customerDao");
+        /*CustomerDao customerDao = (CustomerDao) context.getBean("customerDao");
         customerDao.insert(new Customer(1, "Yudi Setiawan", "Jalan Klambir 5", "kolonel.yudisetiawan@gmail.com"));
         customerDao.insert(new Customer(2, "Dian Ika Wahyuni", "Jalan Marelan pasar 3", "dianika.wahyuni@gmail.com"));
         customerDao.insert(new Customer(3, "Arie Dwi Nugraha", "Jalan Setia Budi", "aridwinugraha@gmail.com"));
@@ -21,6 +21,21 @@ public class App {
         System.out.println(customerLocal);
 
         customerDao.deleteByCustomerId(2);
+        System.out.println("Customer ID 2 has been deleted");*/
+
+        CustomerDao customerDaoSupport = (CustomerDao) context.getBean("customerDaoSupport");
+        customerDaoSupport.insert(new Customer(1, "Yudi Setiawan", "Jalan Klambir 5", "kolonel.yudisetiawan@gmail.com"));
+        customerDaoSupport.insert(new Customer(2, "Dian Ika Wahyuni", "Jalan Marelan pasar 3", "dianika.wahyuni@gmail.com"));
+        customerDaoSupport.insert(new Customer(3, "Arie Dwi Nugraha", "Jalan Setia Budi", "aridwinugraha@gmail.com"));
+
+        Customer customerLocal = customerDaoSupport.findByCustomerId(1);
+        System.out.println(customerLocal);
+
+        customerLocal.setFullName("Valentino Rossi");
+        customerDaoSupport.update(customerLocal);
+        System.out.println(customerLocal);
+
+        customerDaoSupport.deleteByCustomerId(2);
         System.out.println("Customer ID 2 has been deleted");
     }
 }
---
+++ src/main/java/com/tokobuku/dao/impl/JdbcDaoSupportCustomerDao.java
@@ -0,0 +1,53 @@
+package com.tokobuku.dao.impl;
+
+import com.tokobuku.dao.CustomerDao;
+import com.tokobuku.dao.mapper.CustomerRowMapper;
+import com.tokobuku.model.Customer;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class JdbcDaoSupportCustomerDao extends JdbcDaoSupport implements CustomerDao {
+
+    public void insert(Customer customer) {
+        String sql = "INSERT INTO CUSTOMER " +
+                "(cust_id, full_name, address, email) " +
+                "VALUES " +
+                "(?, ?, ?, ?)";
+        if (getJdbcTemplate() != null) {
+            getJdbcTemplate().update(sql, customer.getCustId(), customer.getFullName(), customer.getFullName(), customer.getEmail());
+        } else {
+            throw new RuntimeException("getJdbcTemplate null");
+        }
+    }
+
+    public Customer findByCustomerId(int custId) {
+        String sql = "SELECT * FROM CUSTOMER " +
+                "WHERE " +
+                "cust_id = ?";
+        if (getJdbcTemplate() != null) {
+            return getJdbcTemplate().queryForObject(sql, new Object[]{custId}, new CustomerRowMapper());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(Customer customer) {
+        String sql = "UPDATE CUSTOMER " +
+                "SET " +
+                "full_name = ?, address = ?, email = ? " +
+                "WHERE cust_id = ?";
+        if (getJdbcTemplate() != null) {
+            getJdbcTemplate().update(sql, customer.getFullName(), customer.getAddress(), customer.getEmail(), customer.getCustId());
+        } else {
+            throw new RuntimeException("getJdbcTemplate null");
+        }
+    }
+
+    public void deleteByCustomerId(int custId) {
+        String sql = "DELETE FROM CUSTOMER WHERE cust_id = ?";
+        if (getJdbcTemplate() != null) {
+            getJdbcTemplate().update(sql, custId);
+        } else {
+            throw new RuntimeException("getJdbcTemplate null");
+        }
+    }
+}
